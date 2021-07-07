9df38311e880afef8e23e09b854e36fcc5d34fb8
Gregory <lobster287@gmail.com>
2021-07-08 00:11:07 +0700

    #LAG-8: refactored realization of service which works with the webhook

--- WebApplication/Services/ConfigureWebhook.cs
+++ WebApplication/Services/ConfigureWebhook.cs
@@ -11,42 +11,34 @@ namespace Telegram.Bot.Examples.WebHook.Services
     public class ConfigureWebhook : IHostedService
     {
         private readonly ILogger<ConfigureWebhook> _logger;
-        private readonly IServiceProvider _services;
 
         private readonly TelegramBotConfiguration _botConfig;
 
+        private readonly ConfiguredTelegramBotClient _botClient;
+
+        private readonly string _webhookAddress;
+
         public ConfigureWebhook(ILogger<ConfigureWebhook> logger,
-                                IServiceProvider serviceProvider,
-                                TelegramBotConfiguration configuration)
+                                TelegramBotConfiguration configuration,
+                                ConfiguredTelegramBotClient botClient)
         {
             _logger = logger;
-            _services = serviceProvider;
             _botConfig = configuration;
+            _botClient = botClient;
+            _webhookAddress = @$"{_botConfig.HostAddress}/bot/{_botConfig.BotToken}";
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            using var scope = _services.CreateScope();
-            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
-
-            // Configure custom endpoint per Telegram API recommendations:
-            // https://core.telegram.org/bots/api#setwebhook
-            // If you'd like to make sure that the Webhook request comes from Telegram, we recommend
-            // using a secret path in the URL, e.g. https://www.example.com/<token>.
-            // Since nobody else knows your bot's token, you can be pretty sure it's us.
-            var webhookAddress = @$"{_botConfig.HostAddress}/bot/{_botConfig.BotToken}";
-            _logger.LogInformation("Setting webhook: ", webhookAddress);
-            await botClient.SetWebhookAsync(webhookAddress, cancellationToken: cancellationToken);
+            _logger.LogInformation("Setting webhook: ", _webhookAddress);
+            await _botClient.SetWebhookAsync(_webhookAddress, cancellationToken: cancellationToken);
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
         {
-            using var scope = _services.CreateScope();
-            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
-
-            // Remove webhook upon app shutdown
+            
             _logger.LogInformation("Removing webhook");
-            await botClient.DeleteWebhookAsync(cancellationToken: cancellationToken);
+            await _botClient.DeleteWebhookAsync(cancellationToken: cancellationToken);
         }
     }
 }
