958f7a6d968e14c9b939ea3f11a0428fd8f2fb9d
Gregory <lobster287@gmail.com>
2021-08-09 22:30:39 +0700

    #LAG-7: added a webhook controller.

---
+++ Lagalike.Telegram/Controllers/WebhookController.cs
@@ -0,0 +1,21 @@
+namespace Lagalike.Telegram.Controllers
+{
+    using System.Threading.Tasks;
+
+    using global::Telegram.Bot.Types;
+
+    using Lagalike.Telegram.Services;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromServices] HandleUpdateService handleUpdateService,
+            [FromBody] Update update)
+        {
+            await handleUpdateService.HandleUpdateAsync(update);
+            return Ok();
+        }
+    }
+}
